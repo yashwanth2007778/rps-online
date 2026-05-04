7c40cb78b44b06859c9132c3afc9e43ffe8cfc16
Yashwanth <yashwanthsirimalla6@gamil.com>
2026-05-04 19:32:44 +0530

    ready for production deploy

--- server.js
+++ server.js
@@ -93,8 +93,7 @@ app.get("/", (req, res) => {
   res.send("RPS Server is running 🚀");
 });
 
-const PORT = process.env.PORT || 3000;
-
-server.listen(PORT, () => {
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
 });
